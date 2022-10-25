3bf21a6f1bbe7a2006ea83b2eab986f38702df5e
Arturo Contreras <artharhys@Arturos-Mac-mini.local>
2022-10-25 00:31:58 -0300

    ame2

--- src/components/BootstrapCarousel.js
+++ src/components/BootstrapCarousel.js
@@ -14,8 +14,8 @@ export default function BootstrapCarousel() {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Primera foto</h3>
+              <p>Ame</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -26,8 +26,8 @@ export default function BootstrapCarousel() {
             />
     
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3>Segunda foto</h3>
+              <p>Ame</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -38,9 +38,9 @@ export default function BootstrapCarousel() {
             />
     
             <Carousel.Caption>
-              <h3>Third slide label</h3>
+              <h3>Tercera foto</h3>
               <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                Ame
               </p>
             </Carousel.Caption>
           </Carousel.Item>
